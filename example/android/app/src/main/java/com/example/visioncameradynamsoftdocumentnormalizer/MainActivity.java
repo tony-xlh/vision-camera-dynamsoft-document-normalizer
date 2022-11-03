23f7126043b7feb3b955c9875e5a721f402605f0
xulihang <xulihanghai@163.com>
2022-11-03 11:20:59 +0800

    install @react-navigation

--- example/android/app/src/main/java/com/example/visioncameradynamsoftdocumentnormalizer/MainActivity.java
+++ example/android/app/src/main/java/com/example/visioncameradynamsoftdocumentnormalizer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.visioncameradynamsoftdocumentnormalizer;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -36,5 +38,10 @@ public class MainActivity extends ReactActivity {
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
     }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
