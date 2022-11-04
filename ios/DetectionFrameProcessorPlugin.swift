f753c8cc472dffa539d6faa0b850306f18d40c98
xulihang <xulihanghai@163.com>
2022-11-04 01:54:27 -0700

    add swift files to the plugin project

---
+++ ios/DetectionFrameProcessorPlugin.swift
@@ -0,0 +1,9 @@
+//
+//  DetectionFrameProcessorPlugin.swift
+//  VisionCameraDynamsoftDocumentNormalizer
+//
+//  Created by xulihang on 2022/11/4.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  VisionCameraDynamsoftDocumentNormalizer
+//
+//  Created by xulihang on 2022/11/4.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
