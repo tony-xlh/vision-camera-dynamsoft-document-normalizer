74c233d3d9d3fafc620b90e78969bf69fd471e23
xulihang <xulihanghai@163.com>
2022-11-07 00:29:00 -0800

    iOS: implement the detection frame processor

--- ios/DetectionFrameProcessorPlugin.swift
+++ ios/DetectionFrameProcessorPlugin.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+@objc(DetectionFrameProcessorPlugin)
+public class DetectionFrameProcessorPlugin: NSObject, FrameProcessorPluginBase {
+    private static let context = CIContext(options: nil)
+    @objc
+    public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+            print("Failed to get CVPixelBuffer!")
+            return nil
+          }
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
+            print("Failed to create CGImage!")
+            return nil
+        }
+        
+        var returned_results: [Any] = []
+        let image = UIImage(cgImage: cgImage)
+        
+        let results = try? VisionCameraDynamsoftDocumentNormalizer.ddn.detectQuadFromImage(image)
+        if results != nil {
+            for result in results! {
+                returned_results.append(Utils.wrapDetectionResult(result:result))
+            }
+        }
+        return returned_results
+    }
+}
--- ios/Utils.swift
+++ ios/Utils.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import DynamsoftCore
+import DynamsoftDocumentNormalizer
+
+class Utils {
+    static public func convertBase64ToImage(_ imageStr:String) ->UIImage?{
+        if let data: NSData = NSData(base64Encoded: imageStr, options:NSData.Base64DecodingOptions.ignoreUnknownCharacters)
+        {
+            if let image: UIImage = UIImage(data: data as Data)
+            {
+                return image
+            }
+        }
+        return nil
+    }
+    
+    static func getBase64FromImage(_ image:UIImage) -> String{
+        let dataTmp = image.jpegData(compressionQuality: 100)
+        if let data = dataTmp {
+            return data.base64EncodedString()
+        }
+        return ""
+    }
+    
+    static func wrapDetectionResult (result:iDetectedQuadResult) -> [String: Any] {
+        var dict: [String: Any] = [:]
+        dict["confidenceAsDocumentBoundary"] = result.confidenceAsDocumentBoundary
+        dict["location"] = wrapLocation(location:result.location)
+        return dict
+    }
+    
+    static private func wrapLocation (location:iQuadrilateral?) -> [String: Any] {
+        var dict: [String: Any] = [:]
+        var points: [[String:CGFloat]] = []
+        let CGPoints = location!.points as! [CGPoint]
+        for point in CGPoints {
+            var pointDict: [String:CGFloat] = [:]
+            pointDict["x"] = point.x
+            pointDict["y"] = point.y
+            points.append(pointDict)
+        }
+        dict["points"] = points
+        return dict
+    }
+    
+}
