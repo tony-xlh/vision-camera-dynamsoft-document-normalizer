9fcdbcf96f8e52eb220d522167b0ddd750292033
xulihang <xulihanghai@163.com>
2022-11-06 23:05:23 -0800

    add objective-c files for the frame processing plugins

---
+++ ios/NormalizationFrameProcessorPlugin.m
@@ -0,0 +1,13 @@
+//
+//  NormalizationFrameProcessorPlugin.m
+//  VisionCameraDynamsoftDocumentNormalizer
+//
+//  Created by xulihang on 2022/11/6.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
+
+@interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(detectAndNormalize, NormalizationFrameProcessorPlugin)
+@end
