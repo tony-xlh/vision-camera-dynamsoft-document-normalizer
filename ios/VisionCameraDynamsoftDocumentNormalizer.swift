2ca5f1c42087d3cd8a6f30f296f75b735f6add53
xulihang <xulihanghai@163.com>
2022-11-07 22:15:27 -0800

    iOS: rotate image taken before normalization

--- ios/VisionCameraDynamsoftDocumentNormalizer.swift
+++ ios/VisionCameraDynamsoftDocumentNormalizer.swift
@@ -22,7 +22,20 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
             let points = quad["points"] as! [[String:NSNumber]]
             let quadrilateral = iQuadrilateral.init()
             quadrilateral.points = convertPoints(points)
-            let normalizedImageResult = try VisionCameraDynamsoftDocumentNormalizer.ddn.normalizeFile(path, quad: quadrilateral)
+            let imageURL = URL(fileURLWithPath: path)
+            let image = UIImage(contentsOfFile: imageURL.path)!
+            print("image orientation: ")
+            print(image.imageOrientation.rawValue)
+            print("image width: ")
+            print(image.size.width)
+            var rotatedImage:UIImage!
+            DispatchQueue.main.sync {
+                rotatedImage = imageRotatedByDegrees(image:image, degrees:90, flip:false)
+            }
+            
+            print("rotatedImage width: ")
+            print(rotatedImage.size.width)
+            let normalizedImageResult = try VisionCameraDynamsoftDocumentNormalizer.ddn.normalizeImage(rotatedImage, quad: quadrilateral)
             print("normalized image width: ")
             print(normalizedImageResult.image.width)
             
@@ -69,6 +82,49 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
         return CGPoints
     }
     
+    public func imageRotatedByDegrees(image:UIImage, degrees: CGFloat, flip: Bool) -> UIImage {
+        let radiansToDegrees: (CGFloat) -> CGFloat = {
+            return $0 * (180.0 / CGFloat.pi)
+        }
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat.pi
+        }
+
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: .zero, size: image.size))
+        let t = CGAffineTransform(rotationAngle: degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        bitmap?.translateBy(x: rotatedSize.width / 2.0, y: rotatedSize.height / 2.0)
+
+        //   // Rotate the image context
+        bitmap?.rotate(by: degreesToRadians(degrees))
+
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+
+        bitmap?.scaleBy(x: yFlip, y: -1.0)
+        let rect = CGRect(x: -image.size.width / 2, y: -image.size.height / 2, width: image.size.width, height: image.size.height)
+
+        bitmap?.draw(image.cgImage!, in: rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
     
     @objc(initLicense:withResolver:withRejecter:)
     func initLicense(license:String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
