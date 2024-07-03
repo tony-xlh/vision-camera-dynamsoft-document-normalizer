d6fc4000de529ead30b3abb3ff81e4ef413ff0d9
xulihang <xulihanghai@163.com>
2024-07-03 15:43:42 +0800

    reinit project

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  VisionCameraDynamsoftDocumentNormalizerExample
+//
+
+import Foundation
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,118 +1,31 @@
 import * as React from 'react';
-import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
-import Scanner from './components/Scanner';
-import type { PhotoFile } from 'react-native-vision-camera';
-import * as DDN from "vision-camera-dynamsoft-document-normalizer";
-import Cropper from './components/Cropper';
-import ResultViewer from './components/ResultViewer';
-import { useEffect } from 'react';
+
+import { StyleSheet, View, Text } from 'react-native';
+import { multiply } from 'vision-camera-dynamsoft-document-normalizer';
 
 export default function App() {
-  const [showScanner,setShowScanner] = React.useState(false);
-  const [showCropper,setShowCropper] = React.useState(false);
-  const [showResultViewer,setShowResultViewer] = React.useState(false);
-  const [photoTaken,setPhotoTaken] = React.useState<PhotoFile|undefined>();
-  const [photoPath,setPhotoPath] = React.useState<string>("");
-  const [points,setPoints] = React.useState<DDN.Point[]>([]);
-  const [status,setStatus] = React.useState<string>("Initializing...");
+  const [result, setResult] = React.useState<number | undefined>();
 
-  useEffect(() => {
-    (async () => {
-      let license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="; //one-day public trial
-      let result = await DDN.initLicense(license);
-      console.log("Licesne valid: ");
-      console.log(result);
-      if (result === false) {
-        Alert.alert("DDN","License invalid");
-      }else{
-        setStatus("");
-      }
-    })();
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
   }, []);
 
-  const onPressed = () => {
-    if (status === "Initializing...") {
-      Alert.alert("DDN","Please wait for the initialization.");
-    }else{
-      setShowScanner(true);
-    }
-  }
-
-  const onScanned = (photo:PhotoFile|null) => {
-    if (photo) {
-      setShowScanner(false);
-      setPhotoTaken(photo);
-      setShowCropper(true);
-    }else{
-      Alert.alert("Error","Failed to take a photo. Please try again.");
-      setShowScanner(false);
-    }
-  }
-
-  const renderBody = () => {
-    if (showScanner) {
-      return (
-        <Scanner onScanned={onScanned}></Scanner>
-      )
-    }else if (showCropper){
-      return (
-        <Cropper 
-          photo={photoTaken}
-          onCanceled={()=>{
-            setShowCropper(false);
-            setShowScanner(true);
-          }}
-          onConfirmed={(path,adjustedPoints)=>{
-            setPhotoPath(path);
-            setPoints(adjustedPoints);
-            setShowCropper(false);
-            setShowResultViewer(true);
-          }}
-        ></Cropper>
-      )
-    }else if (showResultViewer){
-      return (
-        <ResultViewer photoPath={photoPath} points={points} 
-          onBack={()=>{
-            setShowResultViewer(false);
-          }}  
-        ></ResultViewer>
-      )
-    }else{
-      return (
-        <>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => onPressed()}
-          >
-            <Text style={styles.buttonText}>Scan Document</Text>
-          </TouchableOpacity>
-          <Text>{status}</Text>
-        </>
-        )
-    }
-  }
   return (
-    <SafeAreaView style={styles.container}>
-      {renderBody()}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  info: {
-    margin: 8,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "rgb(33, 150, 243)",
-    margin: 8,
-    padding: 10,
-  },
-  buttonText:{
-    color: "#FFFFFF",
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
