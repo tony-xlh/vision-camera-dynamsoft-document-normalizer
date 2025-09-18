2e3210ec6f830a11e1bad8249ad3bce021945000
xulihang <xulihanghai@163.com>
2025-09-18 17:13:33 +0800

    update DCV ios to 3.0 and update example

--- ios/VisionCameraDynamsoftDocumentNormalizer.swift
+++ ios/VisionCameraDynamsoftDocumentNormalizer.swift
@@ -1,7 +1,4 @@
-import DynamsoftCore
-import DynamsoftLicense
-import DynamsoftCaptureVisionRouter
-import DynamsoftDocumentNormalizer
+import DynamsoftCaptureVisionBundle
 
 @objc(VisionCameraDynamsoftDocumentNormalizer)
 class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListener  {
@@ -12,7 +9,6 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
     
     override init(){
         super.init()
-        loadTemplate()
     }
     
     @objc(initRuntimeSettingsFromString:withResolver:withRejecter:)
@@ -24,10 +20,6 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
             print("Unexpected error: \(error).")
             resolve(false)
         }
-    }
-    
-    func loadTemplate(){
-        try? VisionCameraDynamsoftDocumentNormalizer.cvr.initSettings("{\"CaptureVisionTemplates\": [{\"Name\": \"Default\"},{\"Name\": \"DetectDocumentBoundaries_Default\",\"ImageROIProcessingNameArray\": [\"roi-detect-document-boundaries\"]},{\"Name\": \"DetectAndNormalizeDocument_Binary\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-binary\"]},{\"Name\": \"DetectAndNormalizeDocument_Gray\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-gray\"]},{\"Name\": \"DetectAndNormalizeDocument_Color\",\"ImageROIProcessingNameArray\": [\"roi-detect-and-normalize-document-color\"]},{\"Name\": \"NormalizeDocument_Binary\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-binary\"]},{\"Name\": \"NormalizeDocument_Gray\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-gray\"]},{\"Name\": \"NormalizeDocument_Color\",\"ImageROIProcessingNameArray\": [\"roi-normalize-document-color\"]}],\"TargetROIDefOptions\": [{\"Name\": \"roi-detect-document-boundaries\",\"TaskSettingNameArray\": [\"task-detect-document-boundaries\"]},{\"Name\": \"roi-detect-and-normalize-document-binary\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-binary\"]},{\"Name\": \"roi-detect-and-normalize-document-gray\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-gray\"]},{\"Name\": \"roi-detect-and-normalize-document-color\",\"TaskSettingNameArray\": [\"task-detect-and-normalize-document-color\"]},{\"Name\": \"roi-normalize-document-binary\",\"TaskSettingNameArray\": [\"task-normalize-document-binary\"]},{\"Name\": \"roi-normalize-document-gray\",\"TaskSettingNameArray\": [\"task-normalize-document-gray\"]},{\"Name\": \"roi-normalize-document-color\",\"TaskSettingNameArray\": [\"task-normalize-document-color\"]}],\"DocumentNormalizerTaskSettingOptions\": [{\"Name\": \"task-detect-and-normalize-document-binary\",\"ColourMode\": \"ICM_BINARY\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-and-normalize-document-gray\",\"ColourMode\": \"ICM_GRAYSCALE\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-and-normalize-document-color\",\"ColourMode\": \"ICM_COLOUR\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect-and-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect-and-normalize\"}]},{\"Name\": \"task-detect-document-boundaries\",\"TerminateSetting\": {\"Section\": \"ST_DOCUMENT_DETECTION\"},\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-detect\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-detect\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-detect\"}]},{\"Name\": \"task-normalize-document-binary\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"ColourMode\": \"ICM_BINARY\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]},{\"Name\": \"task-normalize-document-gray\",\"ColourMode\": \"ICM_GRAYSCALE\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]},{\"Name\": \"task-normalize-document-color\",\"ColourMode\": \"ICM_COLOUR\",\"StartSection\": \"ST_DOCUMENT_NORMALIZATION\",\"SectionImageParameterArray\": [{\"Section\": \"ST_REGION_PREDETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_DETECTION\",\"ImageParameterName\": \"ip-normalize\"},{\"Section\": \"ST_DOCUMENT_NORMALIZATION\",\"ImageParameterName\": \"ip-normalize\"}]}],\"ImageParameterOptions\": [{\"Name\": \"ip-detect-and-normalize\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7}},{\"Name\": \"ip-detect\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0,\"ThresholdCompensation\": 7}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7},\"ScaleDownThreshold\": 512},{\"Name\": \"ip-normalize\",\"BinarizationModes\": [{\"Mode\": \"BM_LOCAL_BLOCK\",\"BlockSizeX\": 0,\"BlockSizeY\": 0,\"EnableFillBinaryVacancy\": 0}],\"TextDetectionMode\": {\"Mode\": \"TTDM_WORD\",\"Direction\": \"HORIZONTAL\",\"Sensitivity\": 7}}]}")
     }
     
     @objc(detectFile:template:withResolver:withRejecter:)
@@ -87,7 +79,7 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
         if template != "" {
             templateName = template
         }else{
-            templateName = "NormalizeDocument_Color"
+            templateName = "NormalizeDocument_Default"
         }
         var returned_result:[String:String] = [:]
 
@@ -112,7 +104,7 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
         let results = capturedResult.items
         if results != nil {
             if results?.count ?? 0 > 0 {
-                let normalizedImageResult:NormalizedImageResultItem = results![0] as! NormalizedImageResultItem
+                let normalizedImageResult:DeskewedImageResultItem = results![0] as! DeskewedImageResultItem
                 let normalizedUIImage = try? normalizedImageResult.imageData?.toUIImage()
                 if config["saveNormalizationResultAsFile"] != nil {
                     if config["saveNormalizationResultAsFile"] as! Bool == true {
@@ -134,18 +126,21 @@ class VisionCameraDynamsoftDocumentNormalizer: NSObject,LicenseVerificationListe
         resolve(returned_result)
     }
     
-    func convertPoints(_ points:[[String:NSNumber]]) -> [CGPoint] {
-        var CGPoints:[CGPoint] = [];
-        for point in points {
-            let x = point["x"]!
-            let y = point["y"]!
-            let intX = x.intValue
-            let intY = y.intValue
-            let cgPoint = CGPoint(x: intX, y: intY)
-            CGPoints.append(cgPoint)
-        }
-        return CGPoints
-    }
+  func convertPoints(_ points: [[String: NSNumber]]) -> [NSValue] {
+      var nsValues: [NSValue] = []
+      
+      for point in points {
+          let x = point["x"]!
+          let y = point["y"]!
+          let intX = x.intValue
+          let intY = y.intValue
+          let cgPoint = CGPoint(x: intX, y: intY)
+          let nsValue = NSValue(cgPoint: cgPoint)
+          nsValues.append(nsValue)
+      }
+      
+      return nsValues
+   }
     
     @objc(initLicense:withResolver:withRejecter:)
     func initLicense(license:String, resolve:@escaping RCTPromiseResolveBlock,reject:@escaping RCTPromiseRejectBlock) -> Void {
