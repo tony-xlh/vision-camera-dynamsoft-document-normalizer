99302169e10919f20dd7dcb69d5f2912c7171ca3
xulihang <xulihanghai@163.com>
2025-10-15 13:06:55 +0800

    chore(example): use runAsync instead of runAtTargetFps

--- example/src/components/Scanner.tsx
+++ example/src/components/Scanner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Alert, Dimensions, Platform, SafeAreaView, StyleSheet } from 'react-native';
-import { Camera, type PhotoFile, runAtTargetFps, useCameraDevice, useCameraDevices, useCameraFormat, useFrameProcessor } from 'react-native-vision-camera';
+import { Camera, type PhotoFile, runAsync, runAtTargetFps, useCameraDevice, useCameraDevices, useCameraFormat, useFrameProcessor } from 'react-native-vision-camera';
 import * as DDN from "vision-camera-dynamsoft-document-normalizer";
 import { Svg, Polygon } from 'react-native-svg';
 import type { DetectedQuadResult } from 'vision-camera-dynamsoft-document-normalizer';
@@ -180,7 +180,7 @@ export default function Scanner(props:ScannerProps) {
     console.log("detect frame");
     console.log(frame.toString());
     if (takenShared.value === false) {
-      runAtTargetFps(3, () => {
+      runAsync(frame,() => {
         'worklet'
         try {
           const results = DDN.detect(frame);
