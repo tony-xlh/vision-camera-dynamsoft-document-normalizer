7816e097546ca567d3925079ddcc78b66e49a2fe
xulihang <xulihanghai@163.com>
2025-10-16 13:50:36 +0800

    update project to react native 0.77.2

--- example/babel.config.js
+++ example/babel.config.js
@@ -4,6 +4,7 @@ const pak = require('../package.json');
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    '@babel/plugin-transform-optional-chaining',
     [
       'module-resolver',
       {
